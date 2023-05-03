e77593f7bd7e14a904d8b0964666e2ef1da8bcc9
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-05-04 02:52:34 +0600

    footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Navbar from "../pages/Navbar";
 import "../../src/index.css";
 import { Outlet } from "react-router-dom";
+import Footer from "../pages/Footer/Footer";
 const Main = () => {
   return (
     <div className="side-container">
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
