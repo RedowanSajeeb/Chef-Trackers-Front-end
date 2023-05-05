022863e1957297e56e45feeb74eb51c8fc1d0b9e
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-05-05 12:35:40 +0600

    pass

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { Button } from "@material-tailwind/react";
+import { Button, alert } from "@material-tailwind/react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../provider/AuthProvider";
 import {
@@ -7,12 +7,13 @@ import {
   GoogleAuthProvider,
   signInWithPopup,
 } from "firebase/auth";
+import { useAlert } from "react-alert";
 const providerGoogle = new GoogleAuthProvider();
 const githubprovider = new GithubAuthProvider();
 const Login = () => {
   const { signInWithEAndPd, user, logout, auth, setReload, setloding } =
     useContext(AuthContext);
-
+const alert = useAlert();
   const [successful, Setsuccessful] = useState("");
   const [error, setError] = useState("");
 
@@ -28,6 +29,14 @@ const Login = () => {
     const formValue = event.target;
     const email = formValue.email.value;
     const password = formValue.password.value;
+    const confirmpassword = formValue.confirmpassword.value;
+    console.log(confirmpassword);
+
+
+if(password !== confirmpassword){
+  setError("password doesn't match");
+   return alert.error("password doesn't match")
+}
 
     signInWithEAndPd(email, password)
       .then((userCredential) => {
@@ -115,8 +124,8 @@ const Login = () => {
         }}
         className=" dark:bg-gray-900"
       >
-        <div className="flex b flex-col items-center justify-center px-6  mx-auto md:h-screen lg:py-0">
-          <div className="w-full bg-white rounded-lg shadow dark:border md:-mt-80 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="flex  flex-col items-center justify-center px-6  mx-auto md:h-screen lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:-mt-48 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Login to your account
@@ -139,7 +148,7 @@ const Login = () => {
                     id="email"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="name@company.com"
-                    required=""
+                    required
                   />
                 </div>
                 <div>
@@ -155,7 +164,23 @@ const Login = () => {
                     id="password"
                     placeholder="••••••••"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required=""
+                    required
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Confirm password
+                  </label>
+                  <input
+                    type="password"
+                    name="confirmpassword"
+                    id="cpassword"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
                   />
                 </div>
                 <div className="flex items-center justify-between">
