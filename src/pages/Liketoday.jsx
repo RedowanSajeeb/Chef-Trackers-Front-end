c8f67e63ad913743f50f3808bb8362c5d04761c0
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-05-05 21:22:09 +0600

    dez

--- src/pages/Liketoday.jsx
+++ src/pages/Liketoday.jsx
@@ -2,9 +2,44 @@ import React from 'react';
 
 const Liketoday = () => {
     return (
-        <div>
-            
+      <div className="side-container ms-5 me-5 mt-14 md:mt-24">
+        <h1 className=" text-amber-800 text-5xl md:mb-10 mb-5 font-bold  fontStyle">
+          What do you like today?
+        </h1>
+        <p className="text-gray-600 ">
+          Consectetur numquam poro nemo veniam eligendi rem adipisci quo modi.
+        </p>
+        <div className="md:grid grid-cols-2 gap-5 md:mt-16 mt-10  ">
+          <div className="bg-gray-50  p-5 mt-5 ">
+            <h3 className="text-2xl font-bold mb-3"> Starters</h3>
+            <p className="text-gray-600">
+              Consectetur numquam poro nemo veniam eligendi rem adipisci quo
+              modi
+            </p>
+          </div>
+          <div className="bg-gray-50 p-5 mt-5 ">
+            <h3 className="text-2xl font-bold mb-3">Main dishes</h3>
+            <p className="text-gray-600">
+              Consectetur numquam poro nemo veniam eligendi rem adipisci quo
+              modi
+            </p>
+          </div>
+          <div className="bg-gray-50 p-5 mt-5">
+            <h3 className="text-2xl font-bold mb-3">Drink</h3>
+            <p className="text-gray-600">
+              Consectetur numquam poro nemo veniam eligendi rem adipisci quo
+              modi
+            </p>
+          </div>
+          <div className="bg-gray-50 p-5 mt-5 ">
+            <h3 className="text-2xl font-bold mb-3">Desserts</h3>
+            <p className="text-gray-600">
+              Consectetur numquam poro nemo veniam eligendi rem adipisci quo
+              modi
+            </p>
+          </div>
         </div>
+      </div>
     );
 };
 
